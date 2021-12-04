f5ece235f468ed8a03f7efaedaad2d09e9ec8af8
MrLishu <2601165031@qq.com>
2021-12-04 09:28:36 +0800

    label data

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ wavename='morl'
 
 # (c1_raw, c1_label), = loadPHMData(data_directory=raw_data_directory, dataset_names=['c1'])
 # c1_resample, rs_rate = resample(c1_raw, sampling_rate=sampling_rate, resample_number=resample_number, step=step, save=True, save_filepath=processed_data_directory + '/' + 'c1_1024_resample.npy')
-c1_resample = np.load(r'processed_data_directory/c1_1024_resample.npy')
+c1_resample = np.load(r'data/processed/PHM2010/c1_1024_resample.npy')
+c1_label = np.load(r'data/processed/PHM2010/c1_label.npy')
 rs_rate = sampling_rate
 c1_cwt = cwt(c1_resample, rs_rate, totalscale=totalscale, wavename=wavename, save=False)
+
 
 exit()
